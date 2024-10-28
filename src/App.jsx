ee2d0d5c5b2ee82018a8af62f6d7ba5585d4371e
BritanyLM <britanylm@gmail.com>
2024-10-28 19:10:59 -0400

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,42 @@
+import { useState } from 'react'
+import './App.css'
+import { puppyList } from './data.js'
+import './index.css'
 
 
+function App() {
+  const [puppies, setPuppies] = useState(puppyList);
+  const [featPupId, setFeatPupId] = useState(null);
 
-    
-      <div>
+  function handleClick() {
+   
+  }
+  const featuredPup = puppies.find((pup) => pup.id === featPupId);
+  return (
+   
+
+      <div className='App'>
+        {puppies.map((puppy) => {
+            return (
+            <p onClick={()=>{ setFeatPupId(puppy.id)}} key={puppy.id}>
+              {puppy.name}</p>
+        )})};
+      
+          
+             
+              {featPupId && (
+        <div>
+          <h2>{featuredPup.name}</h2>
+          <ul>
+            <li>Age: {featuredPup.age}</li>
+            <li>Email: {featuredPup.email}</li>
+          </ul>
+        </div>
         
-        
+      )}
       </div>
-     
-export default App
+              );
+          }
+          
+  
+export default App;
